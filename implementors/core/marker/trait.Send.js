9ef167a17caa8b7c0413892ca6967d65cc764da2
Travis CI User <travis@example.org>
2017-02-20 16:43:33 +0000

    Update documentation

---
+++ implementors/core/marker/trait.Send.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["cslice"] = ["impl&lt;'a, T:&nbsp;<a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Sync.html' title='core::marker::Sync'>Sync</a>&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Send.html' title='core::marker::Send'>Send</a> for <a class='struct' href='cslice/struct.CSlice.html' title='cslice::CSlice'>CSlice</a>&lt;'a, T&gt;","impl&lt;'a, T:&nbsp;<a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Send.html' title='core::marker::Send'>Send</a>&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Send.html' title='core::marker::Send'>Send</a> for <a class='struct' href='cslice/struct.CMutSlice.html' title='cslice::CMutSlice'>CMutSlice</a>&lt;'a, T&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
